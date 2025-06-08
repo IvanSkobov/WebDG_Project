572b6c4278036f8c1e229b760447f40226c6f388
IvanSkobov <LOC-DOC@LIST.RU>
2025-06-08 18:48:56 +0500

    Initial commit

---
+++ ZeroCoder/main/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'main/index.html')
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def services(request):
+    return render(request, 'main/services.html')
+
+def contact(request):
+    return render(request, 'main/contact.html')
