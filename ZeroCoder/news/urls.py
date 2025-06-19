9e0a55fa80ff2a8d21971292ee9d1d23ee470811
IvanSkobov <LOC-DOC@LIST.RU>
2025-06-19 16:35:13 +0500

    readme.txt

--- ZeroCoder/news/urls.py
+++ ZeroCoder/news/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
 	path('', views.home, name='news_home'),
 	path('<int:news_id>', views.news_detail, name='news-detail'),
+	path('create_news/', views.create_news, name='create_news'),
+	# path('news_edit/<int:news_id>', views.news_edit, name='news_edit'),  # Добавьте функцию news_edit в views.py, если потребуется
+	# path('news_delete/<int:news_id>', views.news_delete, name='news_delete'),  # Добавьте функцию news_delete в views.py, если потребуется
 ]
--- ZeroCoder/news/views.py
+++ ZeroCoder/news/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import News_post
+from .forms import NewsForm
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 def home(request):
 	news = News_post.objects.all()
@@ -8,3 +11,29 @@ def home(request):
 def news_detail(request, news_id):
 	news = get_object_or_404(News_post, id=news_id)
 	return render(request, 'news/news_detail.html', {'news': news})
+
+def create_news(request):
+	edit_id = request.GET.get('edit')
+	delete_id = request.GET.get('delete')
+	if delete_id:
+		News_post.objects.filter(id=delete_id).delete()
+		return HttpResponseRedirect(reverse('create_news'))
+
+	if edit_id:
+		instance = get_object_or_404(News_post, id=edit_id)
+	else:
+		instance = None
+
+	if request.method == 'POST':
+		if edit_id:
+			form = NewsForm(request.POST, instance=instance)
+		else:
+			form = NewsForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect(reverse('create_news'))
+	else:
+		form = NewsForm(instance=instance)
+
+	news = News_post.objects.all().order_by('-pub_date')
+	return render(request, 'news/create_news.html', {'form': form, 'news': news, 'edit_id': edit_id})
